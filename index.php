842ef110cf645b148b0cd73a0b6941066fca05b4
SnPux <54323871+SnPux@users.noreply.github.com>
2020-12-08 10:04:20 -0500

    POG
    :3

---
+++ index.php
@@ -0,0 +1,41 @@
+<DOCTYPEHtml>
+    <html>
+    <head>
+<link rel="stylesheet" href="Zerocss.css">
+<link rel="stylesheet" href="background.css">
+<div id="output"></div>
+<!-- Load Babel -->
+<!-- v6 <script src="https://unpkg.com/babel-standalone@6/babel.min.js"></script> -->
+<script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
+<!-- Your custom script here -->
+<script type="text/babel" src="background.js"></script>
+ <script src="countdown.js"></script> 
+ <script>
+ </script>
+    <link rel="shortcut icon" type="image/gif" href="/favicon.gif"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title> Zero </title>
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="Zero" />
+    <meta property="og:description" content="The Official Zero Loader Website!" />
+    <meta property="og:url" content="http://zeroloader.tk/" />
+    <meta property="og:image" content="https://www.reddit.com/r/DarlingInTheFranxx/comments/a9gl6j/pogchamp/" />
+    </head>
+    <body>
+    <canvas></canvas>
+    <p id="two" class="glow"> Zero Loader </p>
+    <div class="wrapper">
+        <p id="one"> Our Discord For announcements, Download the loader </p>
+        <div class="grid-container">
+        <button style="margin:25px;" class="button" onclick="location.href = 'https://discord.gg/'"> </button1>
+        <button style="margin:25px;" class="button button2" onclick="location.href = ''"> </button2>
+        </div>
+    <div id="timer"></div>
+    <p id="one"> Public release. </p>
+     <audio autoplay loop>
+  <source src="background.ogg" type="audio/ogg">
+  Your browser does not support the audio tag.
+</audio> 
+    </div>
+    </body>
+    </html>
