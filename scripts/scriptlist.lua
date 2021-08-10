e5b40b15a66d5976b039a937ec58de6025ed3530
SnPux <54323871+SnPux@users.noreply.github.com>
2021-08-10 10:14:21 -0400

    Update scriptlist.lua

--- scripts/scriptlist.lua
+++ scripts/scriptlist.lua
@@ -1,12 +1,6 @@
 -- shessh 
 -- streaming mode
--- more to come
+-- anti afk
 
-
-
-
-
-
-
-
--- loadstring(game:HttpGet('http://www.zeroloader.tk/Systeam%20public/Streamingmode.lua', true))()
+-- loadstring(game:HttpGet('http://www.zeroloader.tk/Systeam%20public/Streamingmode.lua', true))() -streaming mode script
+-- loadstring(game:HttpGet('http://www.zeroloader.tk/Systeam%20public/anti-afk.lua', true))() -anti afk
