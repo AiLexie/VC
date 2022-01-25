2d0cd3502f2e416e6f4d5fc80bf3c27795a7541d
SnPux <54323871+SnPux@users.noreply.github.com>
2022-01-24 21:13:18 -0500

    Delete scriptlist.lua

--- scripts/scriptlist.lua
+++
@@ -1,6 +0,0 @@
--- shessh 
--- streaming mode
--- anti afk
-
--- loadstring(game:HttpGet('http://www.zeroloader.tk/Systeam%20public/Streamingmode.lua', true))() -streaming mode script
--- loadstring(game:HttpGet('http://www.zeroloader.tk/Systeam%20public/anti-afk.lua', true))() -anti afk
