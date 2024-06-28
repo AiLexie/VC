e6d1a8ebc0b83f12f4df63b546575e6425fd6989
Lexie <54323871+AiLexie@users.noreply.github.com>
2024-06-28 00:02:09 -0400

    Update and rename Streamingmode.lua to spoofer.lua

---
+++ Systeam public/spoofer.lua
@@ -0,0 +1,12 @@
+ Generates (Real Serials) to all Components in your Hardware Permanently (Best Quality )
+ A Factory Reset wont Change Back to Previous Serials (once you've woofed, your new serials lasts forever)
+
+          Tested & Working Games 
+>> Fortnite | Battle Bus Kick & Official Tournaments
+>> Rust | Permanently Banned Behalf of the Game Dev
+>> Apex-Legends | Client Game Account has Banned
+>> Dead by Daylight  | ACCOUNT SUSPENENDED
+>> Call Of Duty Series | Permanently Banned
+>> Playerunknown's Battlegrounds  | restricted account
+>> Battlefield 2042 |
+>> Others
